eab1de4297ff3f1e587600d127c9c86ca7cee20e
Murillo Goulart <murillo.cg@gmail.com>
2017-09-27 21:14:11 -0300

    Ponto inicial da aplicação snakefy.

---
+++ src/main/java/com/rgbsoftware/snakefy/SnakefyApplication.java
@@ -0,0 +1,12 @@
+package com.rgbsoftware.snakefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakefyApplication.class, args);
+	}
+}
